6fcc08d267161b0a56c085c148f9767ffdb5ee3c
BipinSutar <bpn.sutar@gmail.com>
2018-03-10 11:45:05 +0530

    Commit from home laptop for zycus 10 march

--- Practice1/src/bipin/zycus/test/Account.java
+++ Practice1/src/bipin/zycus/test/Account.java
@@ -6,33 +6,42 @@ public class Account {
 	private int[] accounts;
 	private double totalInterest, lowestInterest, highestInterest, avgInterest;
 	int noInterestAccounts;
+	Object lock1 = new Object();
+	Object lock2 = new Object();
+	Object lock3 = new Object();
+	Object lock4 = new Object();
 
 	// public Account() {
 	// // constructor
 	// }
 	public Account(String studentId) {
-		this.studentId = studentId;
-		int max = 0;
-		for (int i = 0; i < studentId.length(); i++) {
-			Character ch = studentId.charAt(i);
-			int ch_v = Character.getNumericValue(ch);
+		synchronized (lock1) {
+			this.studentId = studentId;
+			int max = 0;
+			for (int i = 0; i < studentId.length(); i++) {
+				Character ch = studentId.charAt(i);
+				int ch_v = Character.getNumericValue(ch);
 
-			if (Character.isDigit(ch) && ch_v > max) {
-				max = ch_v;
+				if (Character.isDigit(ch) && ch_v > max) {
+					max = ch_v;
+				}
 			}
+			n = max;
 		}
-		n = max;
 	}
 
 	public double interest(int amount) {
-		double interest = 0.0;
-		if (amount > 1000 && amount <= 5000)
-			interest = 0.025;
-		else if (amount > 5000 && amount <= 10000)
-			interest = 0.05;
-		else if (amount > 10000)
-			interest = 0.08;
-		return interest * amount;
+		synchronized (lock2) {
+
+			double interest = 0.0;
+			if (amount > 1000 && amount <= 5000)
+				interest = 0.025;
+			else if (amount > 5000 && amount <= 10000)
+				interest = 0.05;
+			else if (amount > 10000)
+				interest = 0.08;
+			return interest * amount;
+		}
 	}
 
 	public int[] getAccounts() {
@@ -40,7 +49,9 @@ public class Account {
 	}
 
 	public void setAccounts(int[] accounts) {
-		this.accounts = accounts;
+		synchronized (lock3) {
+			this.accounts = accounts;
+		}
 	}
 
 	public double getTotalInterest() {
@@ -96,18 +107,20 @@ public class Account {
 	}
 
 	public void calculateInterests(double data[]) {
-		totalInterest = 0.0;
-		for (int i = 0; i < data.length; i++) {
-			if (data[i] == 0.0)
-				noInterestAccounts++;
-			if (data[i] < lowestInterest)
-				lowestInterest = data[i];
-			if (data[i] > highestInterest)
-				highestInterest = data[i];
-			totalInterest = totalInterest + data[i];
+		synchronized (lock4) {
+			totalInterest = 0.0;
+			for (int i = 0; i < data.length; i++) {
+				if (data[i] == 0.0)
+					noInterestAccounts++;
+				if (data[i] < lowestInterest)
+					lowestInterest = data[i];
+				if (data[i] > highestInterest)
+					highestInterest = data[i];
+				totalInterest = totalInterest + data[i];
+			}
+			if (data.length > 0)
+				avgInterest = totalInterest / (data.length);
 		}
-		if (data.length > 0)
-			avgInterest = totalInterest / (data.length);
 	}
 
 }
--- Practice1/src/bipin/zycus/test/DiskBackedMapCollectionTest.java
+++ Practice1/src/bipin/zycus/test/DiskBackedMapCollectionTest.java
@@ -3,6 +3,7 @@ package bipin.zycus.test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
@@ -26,7 +27,7 @@ class CustomKey implements Serializable {
 	private int id;
 
 	public CustomKey(int id) {
-		this.id = id;
+		setId(id);
 	}
 
 	@Override
@@ -53,18 +54,26 @@ class CustomKey implements Serializable {
 		return this.id;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
 }
 
 class CumstomkeySerializer<T extends Serializable> {
+	static int obj_count;
+	ObjectInputStream objectInputStream = null;
+	ObjectOutputStream objectOutputStream = null;
+	Object lock1 = new Object();
+	Object lock2 = new Object();
 
-	CumstomkeySerializer() {
+	CumstomkeySerializer() throws IOException {
 		File f = new File(getFileDir());
 		f.delete();
 		f.mkdir();
 	}
 
 	public String getFileDir() {
-		return "CZ";
+		return "../CZ";
 	}
 
 	public String getFileName(int id) {
@@ -80,28 +89,36 @@ class CumstomkeySerializer<T extends Serializable> {
 		return f.length();
 	}
 
-	public void create(int id) {
-		try {
-			ObjectOutputStream objectOutputStream = new ObjectOutputStream(
-					new FileOutputStream(new File(getFileDir(), getFileName(id))));
-			objectOutputStream.writeObject(new CustomKey(id));
-			// objectOutputStream.flush();
-			// objectOutputStream.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+	public void create(int id, Object key) {
+		synchronized (lock1) {
+
+			try {
+				objectOutputStream = new ObjectOutputStream(
+						new FileOutputStream(new File(getFileDir(), getFileName(id))));
+				objectOutputStream.writeObject(key);
+				objectOutputStream.flush();
+				objectOutputStream.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				objectOutputStream = null;
+			}
 		}
 	}
 
 	public CustomKey read(int id) {
-		try {
-			ObjectInputStream objectInputStream = new ObjectInputStream(
-					new FileInputStream(new File(getFileDir(), getFileName(id))));
+		synchronized (lock2) {
+			try {
+				objectInputStream = new ObjectInputStream(new FileInputStream(new File(getFileDir(), getFileName(id))));
 
-			return (CustomKey) objectInputStream.readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
+				return (CustomKey) objectInputStream.readObject();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				objectInputStream = null;
+			}
+			return null;
 		}
-		return null;
 	}
 
 	public boolean fileExists(int id) {
@@ -109,60 +126,70 @@ class CumstomkeySerializer<T extends Serializable> {
 	}
 }
 
+/**
+ * 
+ * @author BipinSutar </br>
+ *         This class will perform operations of adding object into the map. If
+ *         heap memory is full or number capacity to hold the objects into the
+ *         heap memory's map exceeds then it ill serialize or spill the objects
+ *         in the local memory.</br>
+ *         Also this class will also read last 20 objects created and display on
+ *         the console to demonstrate object storing into map as well local
+ *         memory.</br>
+ * 
+ * 
+ *
+ */
 public class DiskBackedMapCollectionTest {
 
-	public static void main(String[] args) {
-		Map<CustomKey, Object> map = new HashMap<CustomKey, Object>();
-		CumstomkeySerializer<CustomKey> serialzer = new CumstomkeySerializer();
-		long memory = 10000;
-		// for (int i = 0; i < 1000000000; i++) {
-		// if (serialzer.getFreeMemory() < serialzer.getObjectSize(i)) {
-		// serialzer.create(i, new CustomKey(i));
-		// }
-		// }
-		long limit = ObjectSizeFetcher.getObjectSize(new CustomKey(10));
-		System.out.println(limit);
-		for (int i = 0; i < 10000000; i++) {
+	public static void main(String[] args) throws IOException {
+		Map<CustomKey, CustomKey> map = new HashMap<CustomKey, CustomKey>();
+		CumstomkeySerializer<CustomKey> serialzer = new CumstomkeySerializer<CustomKey>();
+		/**
+		 * Size of one object
+		 */
+		long oneObj = ObjectSizeFetcher.getObjectSize(new CustomKey(Integer.MAX_VALUE));
+		int exceed = 0;
+		/**
+		 * Number of objects can be save in memory
+		 */
+		long numberObj = serialzer.getFreeMemory() / oneObj;
+		long limit = numberObj + exceed;
+		CustomKey key = new CustomKey(-1);
+		boolean flag = false;
+		/**
+		 * Creating limit + 10 number objects
+		 */
+		for (int i = 0; i < limit + 10; i++) {
+			key.setId(i);
+			if (numberObj - CumstomkeySerializer.obj_count < 0 || flag) {
+				/**
+				 * Spill on the disk
+				 */
+				serialzer.create(i, key);
+			} else {
+				/**
+				 * Storing on heap memory's map
+				 */
+				try {
+					map.put(key, key);
+					CumstomkeySerializer.obj_count++;
+				} catch (OutOfMemoryError e) {
+					flag = true;
+					continue;
+				}
 
-			CustomKey key = null;
-			try {
-				key = new CustomKey(i);
-				map.put(key, i);
-			} catch (OutOfMemoryError e) {
-				serialzer.create(i);
 			}
 		}
-		for (int i = 0; i < 1000; i++) {
-			System.out.println(map.get(serialzer.read(i)));
-		}
-	}
+		/**
+		 * Retrieving last 20 objects If object data not found in heap memory it will
+		 * append "From disk" message else it will print object data
+		 */
+		for (int i = (int) limit - 10; i < limit + 10; i++) {
+			key.setId(i);
 
-	public static void main1(String[] args) {
-		// TODO Auto-generated method stub
-		// Get current size of heap in bytes
-		long heapSize = Runtime.getRuntime().totalMemory();
-		HashMap<Integer, Integer> map = new HashMap<>();
-		// Get maximum size of heap in bytes. The heap cannot grow beyond this size.
-		// Any attempt will result in an OutOfMemoryException.
-		long heapMaxSize = Runtime.getRuntime().maxMemory();
-
-		// Get amount of free memory within the heap in bytes. This size will increase
-		// after garbage collection and decrease as new objects are created.
-		long heapFreeSize = Runtime.getRuntime().freeMemory();
-		int div = 1;
-		System.out.println("Current heap size " + heapSize / (div * div));
-		System.out.println("Max heap size " + heapMaxSize / (div * div));
-		System.out.println("Free heap size " + heapFreeSize / (div * div));
-		for (int i = 0; i < 10000000; i++) {
-			if (Runtime.getRuntime().freeMemory() > 24)
-				map.put(i, i);
+			System.out.println(map.get(key) == null ? serialzer.read(i).getId() + " From disk " : map.get(key).getId());
 		}
-		heapSize = Runtime.getRuntime().totalMemory();
-		heapMaxSize = Runtime.getRuntime().maxMemory();
-		heapFreeSize = Runtime.getRuntime().freeMemory();
-		System.out.println("Current heap size " + heapSize / (div * div));
-		System.out.println("Max heap size " + heapMaxSize / (div * div));
-		System.out.println("Free heap size " + heapFreeSize / (div * div));
 	}
 
 }
